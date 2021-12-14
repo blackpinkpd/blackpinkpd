c4e30f9ef07a7e59122bb8a08530e7ed5a260a84
blackpinkpd <96092666+blackpinkpd@users.noreply.github.com>
2021-12-13 18:53:55 -0600

    Add files via upload

---
+++ js/redirecciones.js
@@ -0,0 +1,21 @@
+function inicio(){
+    location.href="./index.html";
+}
+function lisa(){
+    location.href="lisa.html";
+}
+function jennie(){
+    location.href="jennie.html";
+}
+function jisoo(){
+    location.href="jisoo.html";
+}
+function rose(){
+    location.href="rose.html";
+}
+function lisa(){
+    location.href="lisa.html";
+}
+function blackpink(){
+    location.href="blackpink.html";
+}
